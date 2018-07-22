31813703c82a5975492f4c47c57cfbe6cec3679b
José Massón <jlmasson@espol.edu.ec>
2018-07-22 10:38:50 -0500

    Api - Adonis

---
+++ database/migrations/1532272459912_cinema_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class CinemaSchema extends Schema {
+  up () {
+    this.create('cinemas', (table) => {
+      table.increments()
+      table.string('cinema_name', 100)
+      table.string('ciname_screenshot', 120)
+      table.string('cinema_address', 100)
+      table.string('cinema_phone', 14)
+      table.integer('cinema_seat_capacity')
+      table.text('cinema_details')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('cinemas')
+  }
+}
+
+module.exports = CinemaSchema
